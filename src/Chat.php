e082f6dd20dfa1d72787fed6cd39d7f5148b3050
Mathias Eklöf <mathiaseklof@gmail.com>
2021-12-07 11:48:32 +0100

    Push

--- src/Chat.php
+++ src/Chat.php
@@ -19,16 +19,15 @@ class Chat {
         $response = new Response($request);
         if ($this->client->debug)
         {
-            if ($response->isOkay())
+            if($response->getError())
             {
-                //echo $this->client->config['username'].' ['.$this->channel.']: '.$message.PHP_EOL;
-                return true;
+                echo '[Error] '.$response->getError().'.'.PHP_EOL;
+                return false;
             }
             else
             {
-                echo '[Error] '.$response->getError().'.'.PHP_EOL;
-                echo '[Query] '.var_export($this->client->request('chat.postMessage', $query)->getQuery(), true);
-                return false;
+                echo $this->client->config['username'].' ['.$this->channel.']: '.$message.PHP_EOL;
+                return true;
             }
         }
     }
